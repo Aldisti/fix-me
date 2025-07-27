3c3016831f9cb6247a23c20ff25976fc34fbb47c
aldisti <alessandrodiste99@gmail.com>
2025-07-27 20:53:34 +0200

    refactoring broker logic 🎀

--- fix-me/broker/src/main/java/net/aldisti/broker/BrokerContext.java
+++ fix-me/broker/src/main/java/net/aldisti/broker/BrokerContext.java
@@ -1,6 +1,5 @@
 package net.aldisti.broker;
 
-import net.aldisti.broker.fix.MessageBuilder;
 import net.aldisti.common.fix.Message;
 import net.aldisti.common.fix.constants.Tag;
 import org.slf4j.Logger;
@@ -9,66 +8,78 @@ import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
-public class BrokerContext {
+/**
+ * Broker strategy, implement this class to create a new one.
+ */
+public abstract class BrokerContext {
     private static final Logger log = LoggerFactory.getLogger(BrokerContext.class);
-    public static final int INITIAL_BALANCE = 10000;
 
-    /**
-     * The key is {@code <marketId>|<assetId>}
-     */
-    private final Map<String, TradedAsset> assets;
-    private Integer balance;
+    static final Integer INITIAL_BALANCE = 10000;
+
+    protected final Map<String, TradedAsset> assets;
+    protected Integer balance;
 
     public BrokerContext() {
         assets = new HashMap<>();
         balance = INITIAL_BALANCE;
     }
 
-    public void buy(final Message msg) {
+    /**
+     * Check if there is a need to buy or sell an asset.
+     *
+     * @return a message (with a buy or sell order) to send
+     * to the client or null if there is nothing to do.
+     */
+    abstract Message checkForBuy(final TradedAsset asset);
+
+    /**
+     * Update context after successful buy order.
+     */
+    void executeBuy(final Message msg) {
         assets.get(getKey(msg))
                 .add(msg.getInt(Tag.QUANTITY), msg.getInt(Tag.PRICE));
         log.info("Bought {} shares of {} for {}", msg.get(Tag.QUANTITY), msg.get(Tag.ASSET_ID), msg.get(Tag.PRICE));
     }
 
-    public Message buyOrder(final TradedAsset asset, int quantity) {
-        int cost = asset.getPrice() * quantity;
-        if (cost <= 0 || cost > balance)
-            return null;
-        balance -= cost;
-        return MessageBuilder.buy(asset, quantity, asset.getPrice());
-    }
-
-    public void restoreRejected(final Message msg) {
-        switch (msg.type()) {
-            case BUY -> balance += msg.getInt(Tag.QUANTITY) * msg.getInt(Tag.PRICE);
-            case SELL -> balance -= msg.getInt(Tag.QUANTITY) * msg.getInt(Tag.PRICE);
-        }
-        log.info("Rejected order restored");
-    }
-
-    public void sell(final Message msg) {
+    /**
+     * Update context after successful sell order.
+     */
+    void executeSell(final Message msg) {
         assets.get(getKey(msg))
                 .subtract(msg.getInt(Tag.QUANTITY), msg.getInt(Tag.PRICE));
         balance += msg.getInt(Tag.PRICE) * msg.getInt(Tag.QUANTITY);
         log.info("Sold {} shares of {} for {} each", msg.get(Tag.QUANTITY), msg.get(Tag.ASSET_ID), msg.get(Tag.PRICE));
     }
 
-    public final TradedAsset addOrUpdate(final Message msg) {
+    /**
+     * Restore context after rejected order.
+     */
+    void restoreOrder(final Message msg) {
+        switch (msg.type()) {
+            case BUY -> balance += msg.getInt(Tag.QUANTITY) * msg.getInt(Tag.PRICE);
+            case SELL -> balance -= msg.getInt(Tag.QUANTITY) * msg.getInt(Tag.PRICE);
+        }
+        log.warn("Rejected order restored");
+    }
+
+    /**
+     * Updates an existing asset or creates one.
+     */
+    TradedAsset updateAsset(final Message msg) {
         return assets.compute(getKey(msg), (k, v) ->
                 (v == null) ? new TradedAsset(msg) : v.update(msg.getInt(Tag.PRICE)));
     }
 
-    public Integer getNetWorth() {
+    /**
+     * Calculate and return the net worth of the broker.
+     */
+    Integer getNetWorth() {
         return balance + assets.values().parallelStream()
                 .reduce(0, (partial, asset) ->
                         partial + asset.getPrice() * asset.getQuantity(), Integer::sum);
     }
 
-    private static String getKey(final Message msg) {
+    static String getKey(final Message msg) {
         return msg.get(Tag.SENDER_ID) + "|" + msg.get(Tag.ASSET_ID);
-    }
-
-    public Integer getBalance() {
-        return this.balance;
     }
 }
---
+++ fix-me/broker/src/main/java/net/aldisti/broker/SimpleBrokerContext.java
@@ -0,0 +1,39 @@
+package net.aldisti.broker;
+
+import net.aldisti.broker.fix.MessageBuilder;
+import net.aldisti.common.fix.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SimpleBrokerContext extends BrokerContext {
+    private static final Logger log = LoggerFactory.getLogger(SimpleBrokerContext.class);
+
+    @Override
+    public Message checkForBuy(final TradedAsset asset) {
+        if (asset.getQuantity() == 0) {
+            // check current asset allocation before buying
+            return buyOrder(asset, 10);
+        }
+
+        int ror = asset.rateOfReturn();
+        if (ror >= 5 && asset.getQuantity() / 4 > 0) {
+            return MessageBuilder.sell(asset, asset.getQuantity() / 4, asset.getPrice());
+        } else if (ror <= -5) {
+            int quantity = (asset.getQuantity() / 4 > 0) ? asset.getQuantity() / 4 : 10;
+            return buyOrder(asset, quantity);
+        } else {
+            return null;
+        }
+    }
+
+    private Message buyOrder(final TradedAsset asset, int quantity) {
+        int cost = asset.getPrice() * quantity;
+        if (cost <= 0 || cost > balance)
+            return null;
+        balance -= cost;
+        return MessageBuilder.buy(asset, quantity, asset.getPrice());
+    }
+}
