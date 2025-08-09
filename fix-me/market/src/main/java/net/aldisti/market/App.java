4339bd8e3072cef633205219f5bb8303f7fc5941
aldisti <alessandrodiste99@gmail.com>
2025-08-09 09:23:59 +0200

    handling cannot connect exception in market app

--- fix-me/market/src/main/java/net/aldisti/market/App.java
+++ fix-me/market/src/main/java/net/aldisti/market/App.java
@@ -1,17 +1,28 @@
 package net.aldisti.market;
 
 import net.aldisti.common.network.Client;
+import net.aldisti.common.network.InvalidClientConnection;
 import net.aldisti.common.network.SharedQueueClient;
 import net.aldisti.market.db.Database;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import sun.misc.Signal;
 
 public class App {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
         Market market = createMarket(args);
 
         Database.create();
-
-        Client client = new SharedQueueClient("localhost", 5001, market.getQueue());
+        Client client;
+        try {
+            client = new SharedQueueClient("localhost", 5001, market.getQueue());
+        } catch (InvalidClientConnection e) {
+            log.error("Client connection failed", e);
+            Database.close();
+            return;
+        }
         Thread.ofVirtual().start(client);
 
         Signal.handle(new Signal("INT"), (signal) -> {
